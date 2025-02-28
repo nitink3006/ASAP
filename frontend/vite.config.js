596d9325cdceb997609777c0d2b4a7b91f99a554
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2025-02-28 17:36:24 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/", // Ensure correct base path
   plugins: [react(), tailwindcss()],
+  server: {
+    historyApiFallback: true, // Fix for Vercel route refresh issue
+  },
 });
