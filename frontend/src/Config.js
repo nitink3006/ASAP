873c538b5d20db6e978349320c250bbf9ee6e283
sarthak <sarthaks2711btechcse2025@kccitm.edu.in>
2025-06-13 10:44:54 +0530

    done

--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,8 +1,8 @@
 const Config = {
-    // API_URL: "https://urbanclean.pythonanywhere.com/api",
-    // MEDIA_URL: "https://urbanclean.pythonanywhere.com",
-    API_URL: "https://cod.crpch.in/api",
-    MEDIA_URL: "https://cod.crpch.in",
+    API_URL: "https://urbanclean.pythonanywhere.com/api",
+    MEDIA_URL: "https://urbanclean.pythonanywhere.com",
+    // API_URL: "https://cod.crpch.in/api",
+    // MEDIA_URL: "https://cod.crpch.in",
 };
 
 export default Config;
